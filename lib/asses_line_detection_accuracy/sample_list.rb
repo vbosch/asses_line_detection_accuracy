84335f8e4c41da64c707fa62ba86130e60551d9b
vbosch <vbosch@gmail.com>
2011-10-15 10:07:34 +0200

    initial version os sample list tested

--- lib/asses_line_detection_accuracy/sample_list.rb
+++ lib/asses_line_detection_accuracy/sample_list.rb
@@ -1,9 +1,11 @@
 module AssesLineDetectionAccuracy
+  require 'ap'
   class SampleList
     def initialize(ex_file_name)
       @file_name = ex_file_name
       @list = Array.new
       @processed = Hash.new{false}
+      read
     end
 
     def read
@@ -12,18 +14,18 @@ module AssesLineDetectionAccuracy
           process_line(line)
         end
       end
+      ap @list
     end
 
     def process_line(line)
-
-      @list.push
+      @list.push(line.split[0].split("/")[1])
     end
 
     def processed?(x)
       @processed[x]
     end
 
-    def process(x)
+    def mark_as_processed(x)
       raise "Element already processed" if processed? x
       raise "Element not in list" unless include? x
       @processed[x]=true
